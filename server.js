d7236bed68137f07e7d6d25f374f545b4837abf9
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-31 15:57:38 -0700

    Add deploy code

--- server.js
+++ server.js
@@ -1,33 +1,59 @@
 'use strict';
 
-let cron = require('node-cron');
-
 // DEPLOY STUFF:
-// var app = require('express')();
-// var server = require('http').Server(app);
-// const io = require('socket.io')(server);
-// server.listen(process.env.PORT);
+var app = require('express')();
+var server = require('http').Server(app);
+const io = require('socket.io')(server);
+server.listen(process.env.PORT);
 
-const io = require('socket.io')(3006);
-
-let arr = [];
-
-io.on('connection', socket => {
+io.on('connection', function(socket) {
   console.log(`Connection from: ${socket.id}`);
 
   // emit data
   socket.on('moisture-data', payload => {
     let newPayload = JSON.parse(payload);
-    arr.push(newPayload);
 
-    io.emit('moisture-data', payload);
+    io.emit('moisture-data', newPayload);
   });
 
   // database data
-  // cron.schedule('*/1 * * * *', function() {
-  //   console.log('array', arr);
-  //   io.emit('database-data', arr[arr.length-1]);
+  // Get average and send
+  // let job = cron.schedule('0 */2 * * * *', function() {
+  //   let newArr = arr;
+  //   let length = newArr.length;
   //   arr = [];
+
+  //   let total = newArr.reduce((acc, cur) => {
+  //     acc += cur.moistureNumber;
+
+  //     return acc;
+  //   }, 0);
+
+  //   total = total / length;
+
+  //   let obj = {moistureNumber: total, timestamp: new Date()};
+    
+  //   io.emit('database-data', obj);
+  // }, {
+  //   scheduled: true,
+  // });
+
+  // setInterval(function(){ 
+  //   let newArr = arr;
+  //   let length = newArr.length;
+  //   arr = [];
+
+  //   let total = newArr.reduce((acc, cur) => {
+  //     acc += cur.moistureNumber;
+
+  //     return acc;
+  //   }, 0);
+
+  //   total = total / length;
+
+  //   let obj = {moistureNumber: total, timestamp: new Date()};
+    
+  //   io.emit('database-data', obj);
   // });
 
   socket.on('req-data', payload => {
